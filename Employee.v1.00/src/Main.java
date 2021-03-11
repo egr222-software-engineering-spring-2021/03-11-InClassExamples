ada3025f7f40c0be11816cc8ec819137e9782086
Larry Clement <34249201+lwclement@users.noreply.github.com>
2021-03-10 21:22:41 -0800

    After removing 03/09 code

--- Employee.v1.00/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-	    Employee e = new Employee();
-	    Secretary s = new Secretary();
-
-	    s.takeDictation("Meeting notes");
-    }
-}
